121dac3eaef4d8978a011bc2c4cf8902f19b02e2
PGH <11803923+pediRAM@users.noreply.github.com>
2023-05-18 15:44:40 +0200

    V1.0.1

--- ConsoleApp/ConsolePrinter.cs
+++ ConsoleApp/ConsolePrinter.cs
@@ -190,7 +190,7 @@ Try 'dfp --help' for more information.";
         {
             return @"
 **********************************************************
-*** Directory FingerPrinting (dfp) version: 1.0.1-beta ***
+*** Directory FingerPrinting (dfp) version: 1.0.1      ***
 **********************************************************
 Copyright (C) 2023 Pedram GANJEH HADIDI.
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
